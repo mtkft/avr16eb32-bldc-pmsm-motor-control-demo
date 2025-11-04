7dbeb6f4f027d858d4d30be19c95ab29a50abc2c
Mákos Tészta Kft. <63917881+mtkft@users.noreply.github.com>
2025-11-04 12:04:18 -0500

    Reconfig mc_pins.h for AVR16EB28 SPDIP application
    + preemptively added I2C client-related defs
    * marked potentiometer/PWM speed input as deprecated but did not remove. TODO: the important part in the actual code for interpreting I2C instead
    * moved Hall sensors to PortD2:0. TODO: activation level for the B phase sensor

--- apps/mc_demo.X/mc_pins.h
+++ apps/mc_demo.X/mc_pins.h
@@ -104,4 +104,81 @@
 #define PWM_CL_PIN          PIN5_bm
 #endif  /* __AVR16EB32__ */
 
+/** MODS for TEACar v2 propulsion -- AVR16EB28 and some SPDIP-motivated pin assmts **/
+
+#ifdef  __AVR16EB28__
+
+// LED and buttons if we were bothered
+#define LED_PORT            PORTF
+#define LED_PIN             PIN5_bm
+#define BUTTON_PORT         PORTF
+#define BUTTON_PIN          PIN6_bm
+#define BUTTON_CTRL         PIN6CTRL
+
+// Hall sensors
+#define HA_PORT             PORTD
+#define HA_PIN              PIN0_bm
+#define HB_PORT             PORTD
+#define HB_PIN              PIN1_bm
+#define HC_PORT             PORTD
+#define HC_PIN              PIN2_bm
+
+// back-emf if we were bothered
+#define PHASE_A_PORT        PORTD
+#define PHASE_A_CTRL        PIN4CTRL
+#define PHASE_A_ADC_PIN     ADC_MUXPOS_AIN4_gc
+#define PHASE_A_AC_PIN      AC_MUXPOS_AINP5_gc
+#define PHASE_B_PORT        PORTD
+#define PHASE_B_CTRL        PIN5CTRL
+#define PHASE_B_AC_PIN      AC_MUXPOS_AINP6_gc
+#define PHASE_C_PORT        PORTD
+#define PHASE_C_CTRL        PIN6CTRL
+#define PHASE_C_AC_PIN      AC_MUXPOS_AINP3_gc
+#define PHASE_N_PORT        PORTD
+#define PHASE_N_CTRL        PIN0CTRL
+#define PHASE_N_AC_PIN      AC_MUXNEG_AINN1_gc
+#define CMP_OUT_PORT        PORTA
+#define CMP_OUT_PIN         PIN7_bm
+
+// analogs but we don't need most of these
+#define VBUS_PORT           PORTD
+#define VBUS_CTRL           PIN4CTRL
+#define VBUS_ADC_PIN        ADC_MUXPOS_AIN20_gc
+// Potentiometer superseded by I2C
+#define POT_PORT            PORTD
+#define POT_CTRL            PIN1CTRL
+#define POT_ADC_PIN         ADC_MUXPOS_AIN1_gc
+// I2C isn't technically analog but it can live here near its predecessor
+#define I2C_PORT            PORTC
+#define I2C_CTRLA           TWI_CTRLA
+#define I2C_SADDR           TWI_SADDR
+#define I2C_SCTRLA          TWI_SCTRLA
+#define TEMP_PORT           PORTD
+#define TEMP_CTRL           PIN0CTRL
+#define TEMP_ADC_PIN        ADC_MUXPOS_AIN28_gc
+#define CRT_SNS_PORT        PORTD
+#define CRT_SNS_CTRL        PIN3CTRL
+#define CRT_SNS_ADC_PIN     ADC_MUXPOS_AIN3_gc
+#define CRT_REF_PORT        PORTD
+#define CRT_REF_CTRL        PIN7CTRL
+#define CRT_REF_ADC_PIN     ADC_MUXPOS_AIN7_gc
+#define CRT_FAULT_AC_PIN    AC_MUXPOS_AINP1_gc
+
+// lol the actual bridge outputs
+#define PWM_AH_PORT         PORTA
+#define PWM_AH_PIN          PIN0_bm
+#define PWM_AL_PORT         PORTA
+#define PWM_AL_PIN          PIN1_bm
+#define PWM_BH_PORT         PORTA
+#define PWM_BH_PIN          PIN2_bm
+#define PWM_BL_PORT         PORTA
+#define PWM_BL_PIN          PIN3_bm
+#define PWM_CH_PORT         PORTA
+#define PWM_CH_PIN          PIN4_bm
+#define PWM_CL_PORT         PORTA
+#define PWM_CL_PIN          PIN5_bm
+// no change. it seems this should just work but we can adjust before printing
+
+#endif // __AVR16EB28__
+
 #endif /* MC_PINS_H */
